21c035ff6280be4e256877343c444db54e8b69ee
Aarish Saxena <aarish.work@gmail.com>
2017-04-06 20:11:44 +0530

    Added RD & SD Prog

--- DemoServlet/src/com/aarish/demo/ServletDemo.java
+++ DemoServlet/src/com/aarish/demo/ServletDemo.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class FirstServlet
  */
 
-public class FirstServlet extends HttpServlet {
+public class ServletDemo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
